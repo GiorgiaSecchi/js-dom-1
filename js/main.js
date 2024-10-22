c363224f7a63ce3871a8898e24da0bff282a7df8
GiorgiaSecchi <secchigrg@gmail.com>
2024-10-22 15:15:47 +0200

    get js img + btn

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-console.log("JS OK");
+const imgLamp = document.getElementsByTagName("img");
+const buttonLamp = document.getElementsByTagName("button");
+console.log(imgLamp);
+console.log(buttonLamp);
